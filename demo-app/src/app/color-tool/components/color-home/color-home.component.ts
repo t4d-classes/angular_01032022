2e6b634bda85a83d35229e19c4fb934f2687806c
Eric Greene <eric@t4d.io>
2022-01-03 11:57:04 -0500

    add exercise 1

---
+++ demo-app/src/app/color-tool/components/color-home/color-home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // css selector
+  selector: 'app-color-home',
+  templateUrl: './color-home.component.html',
+  styleUrls: ['./color-home.component.css'],
+})
+export class ColorHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
