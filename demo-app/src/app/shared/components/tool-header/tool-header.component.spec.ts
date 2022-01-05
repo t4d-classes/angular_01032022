5fdd8076cea16ef74d03b9f8aef81cc3d35f5ebd
Eric Greene <eric@t4d.io>
2022-01-05 11:46:21 -0500

    add code demos

--- demo-app/src/app/shared/components/tool-header/tool-header.component.spec.ts
+++ demo-app/src/app/shared/components/tool-header/tool-header.component.spec.ts
@@ -2,9 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToolHeaderComponent } from './tool-header.component';
 
+import { getInnerHtml } from '../../utils/test-utils';
+
 describe('ToolHeaderComponent', () => {
+
   let component: ToolHeaderComponent;
   let fixture: ComponentFixture<ToolHeaderComponent>;
+
+  const theHeaderText = "My Tool";
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +21,20 @@ describe('ToolHeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ToolHeaderComponent);
     component = fixture.componentInstance;
+
+    component.headerText = theHeaderText;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('display the specified header text', () => {
+
+    expect(
+      getInnerHtml(fixture.nativeElement, "h1")
+    ).toBe(theHeaderText);
   });
 });
