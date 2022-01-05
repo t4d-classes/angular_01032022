4bb05a09f60288cbec874b407237e90bb1236061
Eric Greene <eric@t4d.io>
2022-01-05 15:14:33 -0500

    add unit testing demos

--- demo-app/src/app/car-tool/components/car-edit-row/car-edit-row.component.spec.ts
+++ demo-app/src/app/car-tool/components/car-edit-row/car-edit-row.component.spec.ts
@@ -1,25 +1,66 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { Car } from '../../models/cars';
 import { CarEditRowComponent } from './car-edit-row.component';
 
 describe('CarEditRowComponent', () => {
   let component: CarEditRowComponent;
   let fixture: ComponentFixture<CarEditRowComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CarEditRowComponent ]
+  const car: Car = {
+    id: 1,
+    make: 'Ford',
+    model: 'Fusion',
+    year: 2019,
+    color: 'red',
+    price: 45000,
+  };  
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CarEditRowComponent ],
+      imports: [ ReactiveFormsModule ],
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CarEditRowComponent);
     component = fixture.componentInstance;
+    component.car = car;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should output the modified car when save is clicked', () => {
+
+    // move to a utility function
+    const input = fixture.debugElement
+      .query(By.css("td:nth-child(2) > input"))
+      .nativeElement as HTMLInputElement;
+
+    input.value = "Chevrolet";
+    input.dispatchEvent(new Event("input"));
+
+    const spy = jasmine.createSpy();
+    component.saveCar.subscribe(spy);
+
+    // move this to a utility function as well
+    const saveCarButton = fixture.debugElement
+      .query(By.css("button:first-child"))
+      .nativeElement as HTMLButtonElement;
+
+    saveCarButton.dispatchEvent(new Event("click"));
+
+    expect(spy).toHaveBeenCalledWith({
+      ...car,
+      make: 'Chevrolet',
+    });
+
   });
 });
--- demo-app/src/app/car-tool/services/cars.service.spec.ts
+++ demo-app/src/app/car-tool/services/cars.service.spec.ts
@@ -1,16 +1,63 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { CarsService } from './cars.service';
+import { Car } from '../models/cars';
 
 describe('CarsService', () => {
-  let service: CarsService;
+
+  let http: HttpTestingController;
+  let carsSvc: CarsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CarsService);
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+    });
+
+    carsSvc = TestBed.inject(CarsService);
+    http = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(carsSvc).toBeTruthy();
   });
+
+  it("should get all of the cars", () => {
+
+    const expectedCars: Car[] = [
+      {
+        "id": 1,
+        "make": "Ford",
+        "model": "Fusion Hybrid",
+        "year": 2019,
+        "color": "blue",
+        "price": 45000
+      },
+      {
+        "id": 2,
+        "make": "Tesla",
+        "model": "S",
+        "year": 2018,
+        "color": "red",
+        "price": 100000
+      }
+    ];
+
+    const spy = jasmine.createSpy();
+
+    carsSvc.all().subscribe(spy);
+
+    const req = http.expectOne("http://localhost:3060/cars");
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(expectedCars, {
+      headers: { 'Content-Type': 'application/json' },
+      status: 200,
+      statusText: 'OK'
+    });
+
+    expect(spy).toHaveBeenCalledWith(expectedCars);
+
+  });
+
 });
